7525d75b730827d21679de4170ae806fd99d598f
gagan0183 <g.deepsingh1@gmail.com>
2017-07-02 00:11:57 +0530

    added books till july

--- app-book.js
+++ app-book.js
@@ -3,21 +3,21 @@ var fs = require('fs');
 var request = require('sync-request');
 
 textract.fromFileWithPath('AllBooksNames.docx',  {preserveLineBreaks:true}, function(error, text) {
-   fs.readFile('All.txt', {"encoding": "utf-8"}, function(err, data) {
+   fs.readFile('052017.txt', {"encoding": "utf-8"}, function(err, data) {
         if(err) {
             throw err;
         }
         var bookNames = data.split("\n");
         var count = 1;
         bookNames.forEach(function(element) {
-            var parts = element.split("-");
+            var parts = element.split(",");
             var data = {
-                "isbn": "isbn-" + count,
+                "isbn": parts[4].trim(),
                 "bookName": parts[0].trim(),
                 "type": parts[2].trim(),
                 "author": parts[1].trim(),
-                "startDate": "2016-09-09",
-                "completeDate": "2016-09-09",
+                "startDate": parts[3].trim(),
+                "completeDate": parts[3].trim(),
                 "revision": false
             };
             var res = request('POST', 'http://192.168.1.153:8080/book', {
